fbb3fc1667e5e76662db5ae6616ad2b58324906a
mdlawrenceusa <mdlawrence@me.com>
2023-05-25 21:52:50 -0400

    2 files modified

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-video',
-  template: `
-    <video controls>
-      <source [src]="videoUrl" type="video/mp4">
-    </video>
-  `,
+  templateUrl: './video.component.html',
   styleUrls: ['./video.component.css']
 })
 export class VideoComponent {
-  videoUrl: string = 'https://d1ne3nxzty7ofd.cloudfront.net/clients/qcc/aws/s2021/20230514184913.mp4';
+
 }
+
+
+
